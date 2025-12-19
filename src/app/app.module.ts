e037dedeab899906a0c7545278fa89127435fd7e
Magno V Gomes <magnovgomes74@gmail.com>
2025-12-19 11:50:09 -0300

    Continuando a criar os componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-/*Importações Angular Material*/
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatIconModule} from '@angular/material/icon';
-import {MatInputModule} from '@angular/material/input';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatToolbarModule} from '@angular/material/toolbar';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+/* Importações Angular Material */
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+/* Importações de Rotas */
+import { AppRoutingModule } from './app-routing.module';
+import { RestritoRoutingModule } from './restrito/restrito-routing.module';
+
+/* Componentes */
+import { AppComponent } from './app.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { LoginComponent } from './login/login.component';
 import { MenuComponent } from './menu/menu.component';
@@ -22,7 +28,6 @@ import { RestritoComponent } from './restrito/restrito.component';
 import { AtualizaProdutoComponent } from './restrito/atualiza-produto/atualiza-produto.component';
 import { CadastroProdutoComponent } from './restrito/cadastro-produto/cadastro-produto.component';
 import { ListaProdutoComponent } from './restrito/lista-produto/lista-produto.component';
-import { MenuProdutoComponent } from './restrito/menu-produto/menu-produto.component';
 import { MenuRestritoComponent } from './restrito/menu-restrito/menu-restrito.component';
 
 @NgModule({
@@ -36,7 +41,6 @@ import { MenuRestritoComponent } from './restrito/menu-restrito/menu-restrito.co
     AtualizaProdutoComponent,
     CadastroProdutoComponent,
     ListaProdutoComponent,
-    MenuProdutoComponent,
     MenuRestritoComponent
   ],
   imports: [
@@ -50,7 +54,11 @@ import { MenuRestritoComponent } from './restrito/menu-restrito/menu-restrito.co
     MatIconModule,
     MatInputModule,
     MatMenuModule,
-    MatToolbarModule
+    MatToolbarModule,
+    RestritoRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/restrito/lista-produto/lista-produto.component.ts
+++ src/app/restrito/lista-produto/lista-produto.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Produto } from '../../models/Produto.model';
+import { ProdutoService } from '../../produto.service';
 
 @Component({
   selector: 'app-lista-produto',
   templateUrl: './lista-produto.component.html',
   styleUrls: ['./lista-produto.component.css']
 })
-export class ListaProdutoComponent {
+export class ListaProdutoComponent implements OnInit {
 
+  public produtos: Produto[] = [];
+
+  constructor(private _produtoService: ProdutoService, private _router: Router) { }
+
+  ngOnInit(): void {
+    this.listarProdutos();
+  }
+
+  listarProdutos(): void {
+    this._produtoService.getProdutos().subscribe(
+      retornoData => {
+        this.produtos = retornoData.map(item => {
+          return new Produto(
+            item.id,
+            item.produto,
+            item.descricao,
+            item.foto,
+            item.preco
+          );
+        });
+      }
+    )
+  }
+
+  excluir(id: number) {
+    this._produtoService.removerProduto(id).subscribe(
+      produto => {
+        this.listarProdutos();
+      },
+      err => { alert("Erro ao Excluir") }
+    );
+    this._router.navigate(["/restrito/lista"]);
+  }
 }
